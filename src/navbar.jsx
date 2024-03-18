10ba71a086e8efebf4c3e68e68484fe86f7617f4
satish-hebbal <satihhebbal96@gmail.com>
2024-03-18 23:56:16 +0530

    added menu for mobile

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import hcLight from "./assets/hcLight.png"
 import "./App.css"
 import SplitType from 'split-type'
@@ -10,6 +10,8 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(useGSAP);
 
 export default function NavBar(){
+    
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
 
     useGSAP(()=>{
         gsap.from('#navbar',{
@@ -19,10 +21,16 @@ export default function NavBar(){
         })
     })
 
+    const toggleMobileMenu = () => {
+        setShowMobileMenu(!showMobileMenu);
+      };
+
     return(
+        <>
+        <div className="flex flex-col top-0 z-10 sticky">
         <nav className="h-18 md:px-16 md:py-4 px-4 py-2 w-full bg-transparent bg-opacity-50  backdrop-blur-md flex md:justify-between justify-between items-center  top-0 z-10 sticky" id="navbar">
             <div className="flex gap-12 items-center">
-            <span class="md:hidden material-symbols-outlined">
+            <span onClick={toggleMobileMenu} class="md:hidden material-symbols-outlined" >
                 drag_handle
                 </span>
                <img id="logo" className="h-4"  src={hcLight} alt="" />
@@ -37,9 +45,32 @@ export default function NavBar(){
                 <button>Sign Up</button>
                 <button className="ring-1 px-4 rounded-sm bg-sky-500 text-stone-100 items- py-1">Log In</button>
             </div> */}
+
         
         </nav>
-        
+        {showMobileMenu && (
+        <div className="md:hidden bg-white shadow-md py-4 px-6 ">
+          <ul className="flex flex-col gap-2 mx-4 readex-500 text-lg">
+            <li>
+              <a href="#courses">Courses</a>
+            </li>
+            <hr />
+            <li>
+              <a href="#trainer"> Trainer</a>
+            </li>
+            <hr />
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <hr />
+            <li>
+              <a href="#contact"> Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+        </div>
+    </>
         
 
     )
